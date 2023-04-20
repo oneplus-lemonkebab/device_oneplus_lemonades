e4b858ebd63b08254b5da6ffda736a934bfc1bab
RDS-007 <ramandeep392@gmail.com>
2023-04-20 13:23:07 +0530

    lemonades: Dont build some packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := MatLog Gallery2
+LOCAL_OVERRIDES_PACKAGES := MatLog Gallery2 YouTube MetroMusicPlayer arcore talkback SecurityHubPrebuilt MaestroPrebuilt OdadPrebuilt KidsSupervisionStub CarrierLocation BetterBugStub ANGLE Aperture Chrome
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
